8b2b5e6009dc01a479a9f8058632ead8967de7a5
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-06-30 16:55:27 +0200

    tools: Support concept set version, projects in data_dictionary module
    Also inject category and subcategory metadata when loading resolved
    concept sets.

--- tools/data_dictionary/__init__.py
+++ tools/data_dictionary/__init__.py
@@ -8,6 +8,10 @@ REPOSITORY_URL  = "https://github.com/indicate-eu/data-dictionary"
 CACHE_DIRECTORY = pathlib.Path(xdg_cache_home())
 CLONE_DIRECTORY = CACHE_DIRECTORY / 'indicate' / 'data-dictionary'
 
+VERSIONS_FILE = "concept_sets_versions.json"
+
+PROJECTS_DIRECTORY = "projects"
+
 data_dictionary_commit = None
 
 def ensure_data_dictionary_clone():
@@ -53,7 +57,34 @@ def load_resolved_concept_sets(add_names=True):
             with open(f"{directory}/concept_sets/{concept_set_id}.json") as file:
                 concept_set = json.load(file)
                 resolved_concept_set['name'] = concept_set.get('name')
-                resolved_concept_set['deprecated'] = concept_set.get('metadata', {}).get('reviewStatus') == 'deprecated'
+                metadata = concept_set.get('metadata', {})
+                resolved_concept_set['deprecated'] = metadata.get('reviewStatus') == 'deprecated'
+                en = metadata.get('translations', {}).get('en', {})
+                resolved_concept_set['category'] = en.get('category', None)
+                resolved_concept_set['subcategory'] = en.get('subcategory', None)
         concept_sets[concept_set_id] = resolved_concept_set
     print(f"Read {len(concept_sets)} resolved concept sets at commit {commit}")
     return concept_sets, REPOSITORY_URL, commit
+
+
+def load_concept_set_versions():
+    directory, _ = ensure_data_dictionary_clone()
+    with open(directory / VERSIONS_FILE) as file:
+        return json.load(file)
+
+# Projects
+
+def project_file_name(repository_directory, project_id):
+    return repository_directory / PROJECTS_DIRECTORY / f"{str(project_id)}.json"
+
+
+def load_project(project_id):
+    directory, _ = ensure_data_dictionary_clone()
+    with open(project_file_name(directory, project_id)) as file:
+        return json.load(file)
+
+
+def store_project(project_id, data):
+    directory, _ = ensure_data_dictionary_clone()
+    with open(project_file_name(directory, project_id), 'w') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
